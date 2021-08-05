4d9cfbc73096c92c546e1c8aeb841ef2e0b4504d
om9022 <omkarchikane607@gmail.com>
2021-08-05 14:40:01 +0530

    Added: To edit existing contacts

--- src/com/bridgelabz/addressbookmain/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmain/AddressBookMain.java
@@ -1,10 +1,42 @@
 package com.bridgelabz.addressbookmain;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	public static void main(String[] args) {
-		System.out.println("Welcome to AddressBook Program");
+
+		Scanner scanner = new  Scanner(System.in);
+		System.out.println("Welcome to Address Book");
 		AddressBookService contact = new AddressBookService();
-		contact.addContact();
-		contact.displayContacts();
+		while (true) {
+			System.out.println("Enter what you have to do");
+			System.out.println(" 1 Add Contact \n 2 Edit Contact \n 3 Display Contact \n 4 Quit");
+			int userChoice = scanner.nextInt();
+			switch (userChoice) 
+			{
+			case 1:				
+				contact.addContact();				
+				break;
+			case 2:
+				System.out.println("Enter the Phone Number");
+				int phoneNumber = scanner.nextInt();
+				boolean updated= contact.editContact(phoneNumber);	
+				if(updated)
+				{
+					System.out.println("Contact Updated Successfully");
+				}
+				else
+				{
+					System.out.println("Contact not found");
+				}
+				break;
+			case 3:	
+				contact.diaplayContacts();
+				break;
+			default:
+				System.out.println("Thanks You");
+				System.exit(0);
+			}	
+		}
 	}
 }
