bbfe5a9b8a88b8ef312d270d5a1fd1a818a83ad1
om9022 <omkarchikane607@gmail.com>
2021-08-05 13:00:39 +0530

    Added: new concts addded

--- src/com/bridgelabz/addressbookmain/AddressBookMain.java
+++ src/com/bridgelabz/addressbookmain/AddressBookMain.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.addressbookmain;
 
 public class AddressBookMain {
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook Program");
+		AddressBookService contact = new AddressBookService();
+		contact.addContact();
+		AddressBookService contact2 = new AddressBookService();
+		contact2.addContact();
+		contact.displayContacts();
+		contact2.displayContacts();
+		
+	}
 }
